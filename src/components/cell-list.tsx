282606f747e1caa25d0f17c1dc2ce85f9864a5e2
Serhii Tsyntsar <Serhii.Tsyntsar@tx.local>
2022-08-02 09:46:01 +0300

    Binding React with Redux

---
+++ src/components/cell-list.tsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+import { Cell } from '../state';
+
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }: any) =>
+    order.map((id: string) => data[id])
+  );
+
+  const renderedCells = cells.map((cell: Cell) => (
+    <Fragment key={cell.id}>
+      <CellListItem key={cell.id} cell={cell} />
+      <AddCell forceVisible={false} previousCellId={cell.id} />
+    </Fragment>
+  ));
+
+  return (
+    <div>
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+      {renderedCells}
+    </div>
+  );
+};
+
+export default CellList;
