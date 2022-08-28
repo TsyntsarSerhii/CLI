5e63a512e51d325cecffe7138c75d66315b8bb94
Serhii Tsyntsar <Serhii.Tsyntsar@tx.local>
2022-08-28 16:47:25 +0300

    Adding a Local-Only API

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -1,12 +1,31 @@
-import { serve } from 'local-api';
 import path from 'path';
 import { Command } from 'commander';
+import { serve } from 'local-api';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const serveCommand = new Command()
   .command('serve [filename]')  // brackets [] - optional value 
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')  // brackets <> - requared value
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename)); // calculating file paths
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename)); // calculating file paths
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port.');
+      } else {
+        console.log('Heres the problem', err.message);
+      }
+      process.exit(1);
+    }
   });
