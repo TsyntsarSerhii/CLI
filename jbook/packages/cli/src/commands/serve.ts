c14e52fb62a44b2af8dc9d26a8462565e2bc924c
Serhii Tsyntsar <Serhii.Tsyntsar@tx.local>
2022-09-03 16:24:35 +0300

    fixes for typescript errors in catch block

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -4,11 +4,19 @@ import { serve } from 'local-api';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+interface LocalApiError {
+  code: string;
+}
+
 export const serveCommand = new Command()
   .command('serve [filename]')  // brackets [] - optional value 
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')  // brackets <> - requared value
   .action(async (filename = 'notebook.js', options: { port: string }) => {
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
+
     try {
       const dir = path.join(process.cwd(), path.dirname(filename)); // calculating file paths
       await serve(
@@ -20,11 +28,13 @@ export const serveCommand = new Command()
       console.log(
         `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
       );
-    } catch (err) {
-      if (err.code === 'EADDRINUSE') {
-        console.error('Port is in use. Try running on a different port.');
-      } else {
-        console.log('Heres the problem', err.message);
+   } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "EADDRINUSE") {
+          console.error("Port is in use. Try running on a different port.");
+        }
+      } else if (err instanceof Error) {
+        console.log("Heres the problem", err.message);
       }
       process.exit(1);
     }
--- jbook/packages/local-api/src/router/cells.ts
+++ jbook/packages/local-api/src/router/cells.ts
@@ -8,6 +8,10 @@ interface Cell {
   type: 'text' | 'code';
 }
 
+interface LocalApiError {
+  code: string;
+}
+
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
   router.use(express.json());
@@ -15,15 +19,20 @@ export const createCellsRouter = (filename: string, dir: string) => {
   const fullPath = path.join(dir, filename);
 
   router.get('/cells', async (req, res) => {
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === "string";
+    };
     try {
       // Read the file
       const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
 
       res.send(JSON.parse(result));
-    } catch (err: any) {
-      if (err.code === 'ENOENT') {
-        await fs.writeFile(fullPath, '[]', 'utf-8'); // Add code to create a file and add default cells
-        res.send([]);
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "ENOENT") {
+          await fs.writeFile(fullPath, "[]", "utf-8");
+          res.send([]);
+        }
       } else {
         throw err;
       }
